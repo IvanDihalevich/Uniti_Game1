79ed84b99ea996364010f64dd4ddbe3aa2985978
Ivan Dihalevych <143941559+IvanDihalevich@users.noreply.github.com>
2024-11-11 16:39:15 +0200

    Update

---
+++ src/Lecture2DGame/Assets/Code/MoverX.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Code
+{
+    internal class MoverX : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody2D _rigidbody;
+
+        [SerializeField]
+        private float _speed;
+
+        public void Move(float input)
+        {
+            _rigidbody.velocity = new Vector2(input * _speed, _rigidbody.velocity.y);
+        }
+    }
+}
---
+++ src/Lecture2DGame/Assets/Code/PlayerInputX.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Code
+{
+    public class PlayerInputX : MonoBehaviour
+    {
+        [SerializeField]
+        private MoverX _mover;
+
+        private void Update()
+        {
+            float input = Input.GetAxis("Horizontal");
+            _mover.Move(input);
+        }
+    }
+}
